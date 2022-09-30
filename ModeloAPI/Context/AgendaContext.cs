c4c452b549ba07f9fc264c36ddb1efc33ddbe820
Kathlen <kathlengraziela@gmail.com>
2022-09-30 17:49:20 -0300

    Add project files.

---
+++ ModeloAPI/Context/AgendaContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ModeloAPI.Entities;
+
+namespace ModeloAPI.Context
+{
+    public class AgendaContext : DbContext
+    {
+        public AgendaContext(DbContextOptions<AgendaContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Contato> Contatos { get; set; }
+    }
+}
